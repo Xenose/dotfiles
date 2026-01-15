2a1ddd0a1163516f96c81f80673083b328a615c5
xenose <xenose@live.com>
2026-01-15 22:54:03 +0900

    updating neovim

--- config/nvim/lua/pkgmng.lua
+++ config/nvim/lua/pkgmng.lua
@@ -7,8 +7,11 @@ else
 	PLATFORM = "linux"
 end
 
+print("Neovim platform:", PLATFORM)
+
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
 	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
